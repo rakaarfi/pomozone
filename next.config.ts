7b3720949ecd95e5814c49d79c79a69063b6b81f
rakaarfi <rakaarfi@gmail.com>
2025-07-08 22:52:13 +0800

    change favicon and clean up the code

--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,8 @@ import nextPWA from 'next-pwa';
 const withPWA = nextPWA({
   dest: 'public',
   disable: process.env.NODE_ENV === 'development',
-  register: false, // <-- Biarkan ini tetap false, registrasi manual kita sudah terbukti andal
+  register: false,
   skipWaiting: true,
-  // --- TAMBAHKAN KEMBALI BARIS INI ---
-  // Ini akan memberitahu service worker untuk mengabaikan file yang menyebabkan error 404
   buildExcludes: ["app-build-manifest.json"],
 });
 
